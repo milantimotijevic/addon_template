f031230807a8f9f612bb9f80996cfae87be5e5c8
mtimo101289@gmail.com <mtimo101289@gmail.com>
2019-07-31 00:37:01 +0200

    temporarily hardcoded regrowth instead of cat  form

--- core.lua
+++ core.lua
@@ -1,10 +1,10 @@
 local abilities = {
-    {name = 'Cat Form', button = StanceButton1}
+	--TODO: replace Regrowth with a form for Classic (impossible to test otherwise in BFA because shifing costs no mana)
+    {name = 'Regrowth', button = StanceButton1} 
 };
 
 for i, ability in ipairs(abilities) do
-	--ability.cost = GetSpellPowerCost(ability.name)[1].cost;
-	ability.cost = 15; --TEMPORARILY HARDCODED A MANA VALUE, BECAUSE SHAPESHIFTING IS FREE IN BFA
+	ability.cost = GetSpellPowerCost(ability.name)[1].cost;
     ability.fontString = ability.button:CreateFontString(nil, 'OVERLAY', 'GameTooltipText');
 	ability.fontString:SetPoint("BOTTOM",StanceButton1,"TOP",0,7);
 	ability.fontString:SetText(math.floor(UnitPower('player', 0) / ability.cost * 100)/100);
